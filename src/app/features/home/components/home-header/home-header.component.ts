651d6fc6224abc0b37c382e59f4a3f4ac4cc70fe
Khanh Nguyen <ntkhanh@tma.com.vn>
2026-07-07 12:15:23 +0700

    UI revamp 2nd batch

--- src/app/features/home/components/home-header/home-header.component.ts
+++ src/app/features/home/components/home-header/home-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LoginButtonComponent } from '../../../../shared/components/login-button/login-button.component';
+import { RegisterButtonComponent } from '../register-button/register-button.component';
 
 @Component({
   selector: 'app-home-header',
   standalone: true,
-  imports: [RouterLink, LoginButtonComponent],
+  imports: [RouterLink, LoginButtonComponent, RegisterButtonComponent],
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss'],
 })
