8e327aff3e3d5828fd0d449a165b09c1cdc11887
Khanh Nguyen <ntkhanh@tma.com.vn>
2026-07-07 14:22:36 +0700

    Revamp UI 3rd batch

--- src/app/features/home/components/home-footer/home-footer.component.ts
+++ src/app/features/home/components/home-footer/home-footer.component.ts
@@ -1,16 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { DiscordAuthService } from '../../../../core/services/discord-auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-footer',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, AsyncPipe],
+  imports: [],
   templateUrl: './home-footer.component.html',
   styleUrls: ['./home-footer.component.scss'],
 })
-export class HomeFooterComponent {
-  private readonly authService = inject(DiscordAuthService);
-  readonly currentUser$ = this.authService.currentUser$;
-}
+export class HomeFooterComponent {}
